a9d6f98c0ec2e36f6670f5ad4703c73a693ffe1d
Wattanak Keo <106860261+wattanakkeo@users.noreply.github.com>
2025-03-06 14:15:29 -0800

    - Styled the boxes to have a height and width - Refactored grid generation to a nested for loop

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,16 @@
-// 1. Use the same function and go into each traversal to create 15 more divs
-
+// Generates a 16x16 grid for the etch a sketch using a nested for loop
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 16; i++) {
   const gridBoxRow = document.createElement("div");
   gridBoxRow.classList.add("grid-box-row");
-  gridBoxRow.textContent = "Hi";
   container.appendChild(gridBoxRow);
-  
-  let j = 0;
-  
-  while (j != 15) {
+
+  for (let j = 0; j < 16; j++) {
     const gridBoxColumn = document.createElement("div");
     gridBoxColumn.classList.add("grid-box-column");
-    gridBoxColumn.textContent = "Hi";
     gridBoxRow.appendChild(gridBoxColumn);
-    j += 1;
   }
 }
+
+
