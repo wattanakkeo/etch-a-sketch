23db43ee55963bff55350a5c8acf0f7a0760e82c
Wattanak Keo <106860261+wattanakkeo@users.noreply.github.com>
2025-03-07 12:44:07 -0800

    - Implemented a reset button to allow the user to   reset the grid back to white

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,21 +1,34 @@
 // Generates a 16x16 grid for the etch a sketch using a nested for loop
-const container = document.querySelector(".container");
+function gridGenerator() {
+  const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  const gridBoxRow = document.createElement("div");
-  gridBoxRow.classList.add("grid-box-row");
-  container.appendChild(gridBoxRow);
-
-  for (let j = 0; j < 16; j++) {
-    const gridBoxColumn = document.createElement("div");
-    gridBoxColumn.classList.add("grid-box-column");
-    gridBoxRow.appendChild(gridBoxColumn);
+  for (let i = 0; i < 16; i++) {
+    const gridBoxRow = document.createElement("div");
+    gridBoxRow.classList.add("grid-box-row");
+    container.appendChild(gridBoxRow);
+  
+    for (let j = 0; j < 16; j++) {
+      const gridBoxColumn = document.createElement("div");
+      gridBoxColumn.classList.add("grid-box-column");
+      gridBoxRow.appendChild(gridBoxColumn);
+    }
   }
+  
+  // Alters the box to black when hovered over
+  container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-box-column")) {
+      event.target.style.backgroundColor = "black";
+    }
+  });
+
+  // Resets the entire grid back to white
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  container.replaceChildren();
+  gridGenerator();
+})
 }
 
-// Alters the box to black when hovered over
-container.addEventListener("mouseover", (event) => {
-  if (event.target.classList.contains("grid-box-column")) {
-    event.target.style.backgroundColor = "black";
-  }
-});
+gridGenerator();
+
+
