1bed9bd62258bc4d7236ee943f8c8bc5059a2a19
Wattanak Keo <106860261+wattanakkeo@users.noreply.github.com>
2025-03-07 20:42:57 -0800

    - Implemented the function for grid size to change   the size.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,32 @@
-// Generates a 16x16 grid for the etch a sketch using a nested for loop
-function gridGenerator() {
-  const container = document.querySelector(".container");
+// Allows the user to change the size of the grid
+const DEFAULT_SIZE = 16;
+let size = null;
 
-  for (let i = 0; i < 16; i++) {
+function changeSize() {
+  const gridSize = document.querySelector(".grid-size");
+  gridSize.addEventListener("click", () => {
+    size = prompt("Enter a size 1-100");
+    if (size < 1 || size > 100) {
+      prompt("ERROR: Enter a size 1-100");
+    }
+    container.replaceChildren();
+    gridGenerator(size);
+
+    
+    
+  });
+};
+
+// Generates a 16x16 grid for the etch a sketch using a nested for loop
+const container = document.querySelector(".container");
+
+function gridGenerator(size) {
+  for (let i = 0; i < size; i++) {
     const gridBoxColumn = document.createElement("div");
     gridBoxColumn.classList.add(".grid-box-column");
     container.appendChild(gridBoxColumn);
   
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < size; j++) {
       const gridBoxRow = document.createElement("div");
       gridBoxRow.classList.add("grid-box-row");
       gridBoxColumn.appendChild(gridBoxRow);
@@ -20,18 +39,24 @@ function gridGenerator() {
       event.target.style.backgroundColor = "black";
     }
   });
-
-  // Resets the entire grid back to white
-const reset = document.querySelector("#reset");
-reset.addEventListener("click", () => {
-  container.replaceChildren();
-  gridGenerator();
-})
 }
 
-gridGenerator();
+// Resets the entire grid back to white
+function resetBtn() {
+  const reset = document.querySelector("#reset");
+  reset.addEventListener("click", () => {
+  container.replaceChildren();
+  gridGenerator(DEFAULT_SIZE);
+  });
+}
 
-// 1. Change the grid size function
+gridGenerator(DEFAULT_SIZE);
+resetBtn();
+changeSize();
+
+
+
+
 
 // 2. Generate 17 buttons for colors to select from
 
